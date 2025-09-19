4daaa76b75212049d68fb30d139c3f95412c71d8
Sanji <strawhats@summitbankng.com>
2025-09-19 16:26:05 +0100

    Database Schema Fix

--- models/model.js
+++ models/model.js
@@ -6,7 +6,7 @@ module.exports = {
         address, state, lga, numPos, businessType,
         contactName, accountTypes) {
         const result = await pool.query(
-            `INSERT INTO pos_requests (zone, supervisor, ebizstaff, visited, 
+            `INSERT INTO pos.pos_requests (zone, supervisor, ebizstaff, visited, 
             branch,requestOption, merchantEmail, merchantName, address, state, 
             lga, numPos, businessType, contactName, accountTypes) 
             VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15) RETURNING *`, [zone, supervisor, ebizstaff, visited, branch,
@@ -18,7 +18,7 @@ module.exports = {
 
     async getExcel() {
         const result = await pool.query(
-            'SELECT * FROM pos_requests ORDER BY id DESC'
+            'SELECT * FROM pos.pos_requests ORDER BY id DESC'
         )
         return result.rows;
     }
